e88d344ada2be6512a2bb8eb2d8ad6616b1f9dca
Yuen <yching1@udi.edu.co>
2021-11-18 21:47:47 -0500

    18/11/2021

---
+++ BDMercamax/BDMercamax/ActualizarPuntos.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[ActualizarPuntos]
+	@param1 int,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
--- BDMercamax/BDMercamax/LugarStock.sql
+++ BDMercamax/BDMercamax/LugarStock.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[LugarStock]
 (
-	[id_lugar] INT NOT NULL PRIMARY KEY, 
     [barcode_producto] INT NOT NULL, 
     [cantidad_bodega] INT NOT NULL, 
     [cantidad_gondola] INT NOT NULL, 
